a92625918b9d7e2e4efcd9f013a2c57ada56bbf3
MasiAI1 <mr.fire205@gmail.com>
2024-04-20 18:26:04 +0700

    Добавлен GET запрос по адресу '/'

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/',(req,res) =>{
+    res.send('Hello world')
+})
+
 app.listen(3000,() =>{
     console.log(`Приложение запущено тут: http://localhost:${PORT}`)
 })
