042d5cd34e6b4668c1db5a05ef0cde332abefa98
MasiAI1 <mr.fire205@gmail.com>
2024-04-20 21:05:10 +0700

    Добавлен фронтенд в папку паблик

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express();
+const fs = require('fs').promises
+const path = require('path')
 
 const PORT = 3000;
 
-app.get('/',(req,res) =>{
-    res.send('Hello world')
+app.get('/', (req,res) =>{
+        fs.readFile('./public/index.html', 'utf-8').then((data) => {
+            res.header('Content-Type', 'text/html');
+            res.send(data)
+        })
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(3000,() =>{
     console.log(`Приложение запущено тут: http://localhost:${PORT}`)
