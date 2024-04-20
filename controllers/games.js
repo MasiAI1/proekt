784a858f9c2fb615a27de0891bcca7959b6210c3
MasiAI1 <mr.fire205@gmail.com>
2024-04-20 18:22:08 +0700

    Initial commit

---
+++ controllers/games.js
@@ -0,0 +1,42 @@
+const getAllGames = async (req,res) =>{
+    const games = await readData('./data/games.json')
+    if (!games){
+        res.status = 404;
+        res.send({
+            status: 'error',
+            message:'Нет игр в базе данныхб добавьте игру'
+        })
+        return
+    }
+    res.games = games
+    res.send(req.games)
+}
+const deleteGame = async (req,res) => {
+const games = await readData('./data/games.json')
+    if (!games){
+        res.status = 404;
+        res.send({
+            status: 'error',
+            message:'Нет игр в базе данныхб добавьте игру'
+        })
+        return
+    }
+    req.games = games
+
+    const id = Number(req.params.id)
+
+    req.game = req.games.find((item) => item.id === id)
+
+    const index = req.games.findIndex((item) => item.id === req.game.id)
+
+    req.games.splice(index,1)
+    await writeData('./data/games.json', req.games)
+
+    res.send({
+        games:req.games,
+        updated:
+    })
+
+
+}
+const addGame = async (req,res) => {}
---
+++ routes/main.js
@@ -0,0 +1,5 @@
+const mainRouter = require('express').Router();
+
+mainRouter.get('/',(res.req) =>{
+    fs.read
+})
