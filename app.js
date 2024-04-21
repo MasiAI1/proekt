38c13b0a34497b25dccd13b348f853b2dd514db4
MasiAI1 <mr.fire205@gmail.com>
2024-04-21 13:47:26 +0700

    Добавлена настройка CORS

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express();
-const fs = require('fs').promises
+
 const path = require('path')
+
 const mainRouter = require('./routes/main')
 const gameRouter = require('./routes/games')
+const cors = require('./middlewares/cors')
 
 const PORT = 3000;
+const app = express();
 
 
 app.use(
+    cors,
     bodyParser.json(),
     express.static(path.join(__dirname, 'public')),
     mainRouter,
